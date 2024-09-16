afdb21945515d322e1cc545c0e27f2659846ce9c
vijaykumardas <vijaykumardas@gmail.com>
2024-09-16 13:25:52 +0530

    Update ValueStockJsonParse.py

--- ValueStockJsonParse.py
+++ ValueStockJsonParse.py
@@ -15,7 +15,7 @@ logging.basicConfig(filename="ValueStocksProcess.Log",level=logging.DEBUG,format
 
 
 # Dropbox access token (replace with your actual token)
-DROPBOX_ACCESS_TOKEN = 'sl.B8-Etfrh5db67j0qETTuSo8IHORhTRENqPA8LYq09i5yb6MNTNgIK2J8l5t85L-jgguBxZyOQxkZzEJYKFA_mjOF0rZEqt95LkOu4dYVQ84-YgIsWZ4PyUzyiz4paLiFMZX5uOzCKJYM'
+DROPBOX_ACCESS_TOKEN = 'sl.B9Cw0uLNUhc4XzluiselE75zkiSyau3_l1t1V1_R_Ur_gL0yJuh7LBcBFjgzwjgABj8RClSvbW4HMmAnS12JdP8asblw4IJm_tKvHEkIvFvDVm3w6STMCyAKogPL3f2a0uigVjjvu_Gfy-vSNPm1'
 
 def upload_to_dropbox(file_path, dropbox_path):
     """Uploads the file at file_path to Dropbox at dropbox_path."""
